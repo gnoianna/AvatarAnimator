a44dc0e598156eac9c877bd404de56cfe506bfee
Anna Gnoińska <ania.gnoinska@gmail.com>
2021-11-06 23:27:44 +0100

    clean code

--- app.py
+++ app.py
@@ -1,37 +1,29 @@
 import cv2
 import dlib
 import imutils
-import matplotlib.pyplot as plt
 import numpy as np
 from flask import Flask, render_template, Response, request
 from imutils import face_utils
 from skimage.transform import PiecewiseAffineTransform, warp
 
-global out, detector, predictor, avatar_temp_image, avatar_original_image, animate, file_name, camera
+global detector, predictor, avatar_temp_image, avatar_original_image, animate, file_name, camera
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('./data/shape_predictor_68_face_landmarks.dat')
 
 app = Flask(__name__, template_folder='./templates')
 
 
-def all_inside(avatar, points, src_img, src_points):
+def animate_avatar(avatar_img, avatar_points, src_img, src_points):
     global avatar_temp_image
 
     avatar_new_face = np.zeros(src_img.shape, np.uint8)
-    avatar_img_copy = np.copy(avatar)
-    avatar_img_copy_copy = np.copy(avatar)
-    src_img_copy = np.copy(src_img)
+    avatar_img_copy = np.copy(avatar_img)
 
     for indices in gen_triangles_indices(src_points):
-        avatar_triangle_points = gen_xy_from_indices(points, indices)
+        avatar_triangle_points = gen_xy_from_indices(avatar_points, indices)
         src_triangle_points = gen_xy_from_indices(src_points, indices)
 
-        avatar_triangle, avatar_img_cropped, _ = gen_cropped_triangle(avatar_img_copy_copy, avatar_triangle_points)
-        _, avatar_img_cropped_copy, _ = gen_cropped_triangle(avatar_img_copy, avatar_triangle_points)
-        src_triangle, src_img_cropped, src_b_rect = gen_cropped_triangle(src_img_copy, src_triangle_points)
-
-        x, y, w, h = src_b_rect
+        avatar_triangle, avatar_img_cropped, _ = gen_cropped_triangle(avatar_img_copy, avatar_triangle_points)
+        src_triangle, src_img_cropped, (x, y, w, h) = gen_cropped_triangle(src_img, src_triangle_points)
 
         transform_matrix = cv2.getAffineTransform(np.float32(avatar_triangle), np.float32(src_triangle))
 
@@ -40,38 +32,31 @@ def all_inside(avatar, points, src_img, src_points):
                                          flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT_101)
 
         mask = np.zeros(src_img_cropped.shape, dtype=np.uint8)
-        #avatar_mask = np.zeros(avatar_img_cropped.shape, dtype=np.uint8)
-
         mask = cv2.fillConvexPoly(mask, np.int32(src_triangle), (1.0, 1.0, 1.0), 16, 0)
-        #avatar_mask = cv2.fillConvexPoly(avatar_mask, np.int32(avatar_triangle), (1.0, 1.0, 1.0), 16, 0)
 
-        #src_img_cropped *= 1 - mask
-        #avatar_img_cropped_copy *= 1 - avatar_mask
         avatar_t_warped *= mask
 
-        new_face_rect_area_gray = cv2.cvtColor(avatar_new_face[y: y + h, x: x + w], cv2.COLOR_BGR2GRAY)
-        _, mask_triangles_designed = cv2.threshold(new_face_rect_area_gray, 0, 255, cv2.THRESH_BINARY_INV)
-        avatar_t_warped = cv2.bitwise_and(avatar_t_warped, avatar_t_warped, mask=mask_triangles_designed)
+        new_face_gray = cv2.cvtColor(avatar_new_face[y: y + h, x: x + w], cv2.COLOR_BGR2GRAY)
+        _, mask_triangles = cv2.threshold(new_face_gray, 0, 255, cv2.THRESH_BINARY_INV)
+        avatar_t_warped = cv2.bitwise_and(avatar_t_warped, avatar_t_warped, mask=mask_triangles)
 
         avatar_new_face[y: y + h, x: x + w] += avatar_t_warped
 
     transform = PiecewiseAffineTransform()
-    transform.estimate(points[0:26], src_points[0:26])
-    face = warp(avatar_new_face, transform, output_shape=avatar_img_copy_copy.shape, order=1, mode='wrap')
-
+    transform.estimate(avatar_points[0:35], src_points[0:35])
+    face = warp(avatar_new_face, transform, output_shape=avatar_img.shape, order=1, mode='wrap')
     face = cv2.normalize(face, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
     face_gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-    _, face_mask = cv2.threshold(face_gray, 1, 255, cv2.THRESH_BINARY_INV)
-    new_avatar = cv2.bitwise_and(avatar, avatar, mask=face_mask)
+    _, mask = cv2.threshold(face_gray, 1, 255, cv2.THRESH_BINARY_INV)
+    new_avatar_img = cv2.bitwise_and(avatar_img, avatar_img, mask=mask)
 
-    new_avatar += face
+    new_avatar_img += face
 
-    avatar_temp_image = new_avatar
+    avatar_temp_image = new_avatar_img
 
 
 def gen_frames_with_landmarks():
-    global out, detector, predictor, avatar_temp_image, animate, avatar_original_image
-    print("gen_frames_with_landmarks")
+    global detector, predictor, avatar_temp_image, animate, avatar_original_image
 
     while True:
         success, frame = camera.read()
@@ -84,11 +69,11 @@ def gen_frames_with_landmarks():
                 for rect in rects:
                     camera_shape = predictor(gray, rect)
                     camera_shape = face_utils.shape_to_np(camera_shape)
-                if animate:
-                    animate = 0
-                    avatar_original_image, avatar_shape = gen_landmarks(avatar_original_image, 0)
-                    temp_avatar = np.copy(avatar_original_image)
-                    all_inside(temp_avatar, avatar_shape, frame, camera_shape)
+                    if animate:
+                        animate = not animate
+                        avatar_original_image, avatar_shape = gen_landmarks(avatar_original_image, 0)
+                        temp_avatar = np.copy(avatar_original_image)
+                        animate_avatar(temp_avatar, avatar_shape, frame, camera_shape)
 
         try:
             ret, buffer = cv2.imencode('.jpg', cv2.flip(frame, 1))
@@ -117,37 +102,33 @@ def gen_avatar_stream():
             pass
 
 
-def without_landmarks(image):
-    return np.copy(image)
-
-
-def gen_landmarks(image, if_with_landmarks):
+def gen_landmarks(img, if_with_landmarks):
     global detector, predictor
 
-    without_landmarks_image = without_landmarks(image)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    without_landmarks_img = np.copy(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rects = detector(gray, 1)
 
     for rect in rects:
-        avatar_shape = predictor(gray, rect)
-        avatar_shape = face_utils.shape_to_np(avatar_shape)
+        img_shape = predictor(gray, rect)
+        img_shape = face_utils.shape_to_np(img_shape)
 
-        for i, (x, y) in enumerate(avatar_shape):
-            image = cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+        for i, (x, y) in enumerate(img_shape):
+            img = cv2.circle(img, (x, y), 1, (0, 0, 255), -1)
 
     if if_with_landmarks:
-        return image, avatar_shape
+        return img, img_shape
     else:
-        return without_landmarks_image, avatar_shape
+        return without_landmarks_img, img_shape
 
 
 def gen_triangles_indices(points):
     b_rect = (*points.min(axis=0), *points.max(axis=0))
 
-    subdiv = cv2.Subdiv2D(b_rect)
+    subdiv_object = cv2.Subdiv2D(b_rect)
     points_list = list(points)
-    subdiv.insert(points_list)
-    triangle_list = subdiv.getTriangleList()
+    subdiv_object.insert(points_list)
+    triangle_list = subdiv_object.getTriangleList()
 
     for x1, y1, x2, y2, x3, y3 in triangle_list:
         yield [(points == point).all(axis=1).nonzero()[0][0] for point in [(x1, y1), (x2, y2), (x3, y3)]]
@@ -171,25 +152,24 @@ def gen_xy_from_indices(points, indices):
 def update_avatar():
     global avatar_temp_image, avatar_original_image, file_name
 
-    avatar_temp_image = cv2.imread(file_name)
     avatar_original_image = cv2.imread(file_name)
 
 
 def init():
-    global animate, file_name, camera
+    global predictor, detector, animate, file_name, camera, avatar_temp_image, avatar_original_image
 
+    animate = 0
     camera = cv2.VideoCapture(0)
     file_name = "./static/sim_1.jpg"
-    animate = 0
-    update_avatar()
+    avatar_temp_image = cv2.imread(file_name)
+    avatar_original_image = cv2.imread(file_name)
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('./data/shape_predictor_68_face_landmarks.dat')
 
 
 @app.route('/')
 def index():
-    global animate, avatar_temp_image, avatar_original_image
-
     init()
-
     return render_template('index.html')
 
 
@@ -214,8 +194,9 @@ def tasks():
 
         if request.form.get("avatar"):
             file_name = "." + request.form.get("avatar")
-            update_avatar()
+
         animate = request.form.get("animate")
+        update_avatar()
 
     return render_template('index.html')
 
